54ef073515b387f3c244357f5aaedcb422e0927c
luke <luke.dercher@gmail.com>
2018-04-15 13:02:24 -0500

    sol needs testing

--- HW2/Compiler.hs
+++ HW2/Compiler.hs
@@ -66,7 +66,12 @@ parseTemps (Gep s _ _ _) = [s]
 -- temporaries takes the first string from insts and puts them in list
 
 compileFunction :: Types -> (String, Function) -> X86.Prog
-compileFunction tys (s,f) = error "i"--[(s,False,[])]
+compileFunction tys (n, (args, retT, cfg@(first,rest))) = [(n, True, Text ([pushq ~%RBP, movq ~%RSP ~%RBP, subq ~$(8 * fromIntegral(length tm)) ~%RSP] ++
+                                                            map (\((t,n), reg) -> movq ~%reg ~~(fromJust (lookup n tm))) (zip args argRegs) ++
+                                                            concatMap (\((t,n), i) -> [ movq ~#(8*1 + 8, RBP) ~%RAX, movq ~%RAX ~~(fromJust (lookup n tm))]) (zip (drop 6 args) [1..]) ++
+                                                            compileBlock tys tm first ))] ++ map (\(n,b) -> (n, False, Text (compileBlock tys tm b))) rest
+                                                where tm = map (\(i,n) -> (n, IndBoth (-8 * i) RBP)) (zip [1..] (temporaries cfg ++ map snd args))
+                                                      argRegs = [RDI, RSI, RDX, RCX, R08, R09]
 
 
 --function prologue
@@ -82,7 +87,7 @@ compileFunction tys (s,f) = error "i"--[(s,False,[])]
 --end function 
 
 compileBlock :: Types -> TemporaryMap -> Block -> [X86.SourceInstr]
-compileBlock ts tm (insts,term) = concatMap (compileInstr ts tm) insts ++ (compileTerm tm term)
+compileBlock ts tm (insts,term) = (concatMap (\n -> compileInstr ts tm n) insts) ++ (compileTerm tm term)
 
 compileOperand :: TemporaryMap -> LL.Operand -> X86.SourceOperand
 compileOperand tm (Const i) = Imm (Literal i)
@@ -90,32 +95,31 @@ compileOperand tm (Gid s) = IndImm (Label s)
 compileOperand tm (Uid s) = fromJust (lookup s tm)
 
 compileInstr :: Types -> TemporaryMap -> LL.Instruction -> [X86.SourceInstr]
-compileInstr ty tm (Bin s opator typ opand1 opand2) = binOp (Bin s opator typ opand1 opand2) tm
-compileInstr ty tm (Alloca s t) = [(Subq, [Imm (Literal (sizeOf ty t)),Reg RSP])] --sub rsp by sizeof t store new 
-compileInstr ty tm (Load s t o) = [(Movq,[(fromJust(lookup s tm)), Reg RAX]),(Movq,[IndReg RAX,Reg RAX]),(Movq,[Reg RAX,(compileOperand tm o)])] --move thing pointed to by operand into string
-compileInstr ty tm (Store t o1 o2) = [(Movq, [(compileOperand tm o1),Reg RAX]),(Movq,[(compileOperand tm o2), Reg RCX]),(Movq,[Reg RAX, IndReg RCX])]--move o1 into thing pointed at by o2 
-compileInstr ty tm (Icmp s c t o1 o2) =  [(Movq,[(Imm (Literal 0)),(fromJust(lookup s tm))]),(Cmpq, [(compileOperand tm o1),(compileOperand tm o2)]),(Set c,[(fromJust(lookup s tm))])]--icmp ne i64 %19, note: zero temporary. and  use set to read condition flag and set tempr
-compileInstr ty tm (Call s t s2 tos) = error "i" --sub from rsp appropriate space and callq func name then add back to rsp put first 6 args in regs and then alloc stack space and cleanup when done
-compileInstr ty tm (Bitcast s t o t2) = error "i"--changes operand from type t to type t2 really just a move
-
---cond2Op :: Condition -> X86.SourceOperand
---cond2Op 
-
-binOp :: LL.Instruction -> TemporaryMap -> [X86.SourceInstr]
-binOp (Bin s Add ty opand1 opand2) tm = [(Movq ,[(compileOperand tm opand1),Reg RAX]), (Addq, [(compileOperand tm opand2),Reg RAX]), (Movq, [Reg RAX, (fromJust (lookup s tm))])]
-binOp (Bin s Sub ty opand1 opand2) tm = [(Movq ,[(compileOperand tm opand1), Reg RAX]), (Subq, [(compileOperand tm opand2), Reg RAX]), (Movq, [Reg RAX, (fromJust (lookup s tm))])]
-binOp (Bin s Mul ty opand1 opand2) tm = [(Movq ,[(compileOperand tm opand1), Reg RAX]), (Imulq, [(compileOperand tm opand2), Reg RAX]), (Movq, [Reg RAX, (fromJust(lookup s tm))])]
-binOp (Bin s Shl ty opand1 opand2) tm = [(Movq ,[(compileOperand tm opand1), Reg RAX]), (Shlq, [(compileOperand tm opand2),Reg RAX]), (Movq, [Reg RAX, (fromJust (lookup s tm))])]
-binOp (Bin s Lshr ty opand1 opand2) tm = [(Movq ,[(compileOperand tm opand1), Reg RAX]), (Shrq, [(compileOperand tm opand2),Reg RAX]), (Movq,[Reg RAX, (fromJust (lookup s tm))])]
-binOp (Bin s Ashr ty opand1 opand2) tm = [(Movq ,[(compileOperand tm opand1), Reg RAX]), (Sarq, [(compileOperand tm opand2),Reg RAX]), (Movq,[Reg RAX, (fromJust (lookup s tm))])]
-binOp (Bin s And ty opand1 opand2) tm = [(Movq ,[(compileOperand tm opand1), Reg RAX]), (Andq, [(compileOperand tm opand2),Reg RAX]), (Movq, [Reg RAX, (fromJust (lookup s tm))])]
-binOp (Bin s Or ty opand1 opand2) tm = [(Movq ,[(compileOperand tm opand1), Reg RAX]), (Orq, [(compileOperand tm opand2),Reg RAX]),(Movq,[Reg RAX, (fromJust (lookup s tm))])]
-binOp (Bin s Xor ty opand1 opand2) tm = [(Movq ,[(compileOperand tm opand1), Reg RAX]), (Xorq, [(compileOperand tm opand2),Reg RAX]),(Movq,[Reg RAX, (fromJust (lookup s tm))])]
-  
+compileInstr ty tm (Bin s opator typ opand1 opand2) = [movq ~~(compileOperand tm opand1) ~%RAX, opMap ~~(compileOperand tm opand2) ~%RAX, movq ~%RAX ~~(fromJust (lookup s tm))]
+                                                        where opMap = case opator of
+                                                                            Add -> addq
+                                                                            Sub -> subq
+                                                                            Mul -> imulq
+                                                                            Lshr -> shlq
+                                                                            Ashr -> sarq
+                                                                            And -> andq
+                                                                            Or -> orq
+                                                                            Xor -> xorq
+compileInstr ty tm (Alloca s t) = [subq ~$(sizeOf ty t) ~%RSP, movq ~%RSP ~~(fromJust (lookup s tm))] --sub rsp by sizeof t store new 
+compileInstr ty tm (Load s t o) = [ movq ~~(compileOperand tm o) ~%RAX, movq ~#RAX ~%RAX, movq ~%RAX ~~(fromJust (lookup s tm))] --move thing pointed to by operand into string
+compileInstr ty tm (Store t o1 o2) = [ movq ~~(compileOperand tm o1) ~%RAX, movq ~~(compileOperand tm o2) ~%RCX, movq ~%RAX ~#RCX]--move o1 into thing pointed at by o2 
+compileInstr ty tm (Icmp s c t o1 o2) =  [ movq ~~(compileOperand tm o1) ~%RAX, cmpq ~~(compileOperand tm o2) ~%RAX, set c ~~(fromJust(lookup s tm))]--icmp ne i64 %19, note: zero temporary. and  use set to read condition flag and set tempr
+compileInstr ty tm (Call s t s2 tos) = regI ++ stackI ++ [callq ~$$s2, movq ~%RAX ~~(fromJust(lookup s tm))] --put first 6 args in regs and then alloc stack space and cleanup when done
+                                                  where firstArgRegs = [RDI, RSI, RDX, RCX, R08, R09]
+                                                        regArgs = take 6 tos
+                                                        stackArgs = drop 6 tos
+                                                        regI = map (\(regi, (t,op)) -> movq ~~(compileOperand tm op) ~%regi) (zip firstArgRegs regArgs)
+                                                        stackI = concatMap (\(t,op) -> [movq ~~(compileOperand tm op) ~%RAX, pushq ~%RAX]) stackArgs
+compileInstr ty tm (Bitcast s t o t2) = [ movq ~~(compileOperand tm o) ~%RAX, movq ~%RAX ~~(fromJust(lookup s tm))]--changes operand from type t to type t2 really just a move  
 
 compileTerm :: TemporaryMap -> Terminator -> [X86.SourceInstr]
-compileTerm tm (Ret t Nothing) = [(Retq,[])]
-compileTerm tm (Ret t (Just o)) = [(Movq,[(compileOperand tm o), Reg RAX])]
-compileTerm tm (Bra s) = [(Jmp, [(fromJust (lookup s tm))])]
-compileTerm tm (CBr op s1 s2) = [(Cmpq ,[(compileOperand tm op),Imm (Literal 1)]),(J Eq,[(fromJust(lookup s1 tm))]),(Jmp,[(fromJust(lookup s2 tm))])]
+compileTerm tm (Ret t Nothing) = [movq ~%RBP ~%RSP, popq ~%RBP, retq]
+compileTerm tm (Ret t (Just o)) = [ movq ~~(compileOperand tm o) ~%RAX, movq ~%RBP ~%RSP, popq ~%RBP, retq]
+compileTerm tm (Bra s) = [jmp ~$$ s]
+compileTerm tm (CBr op s1 s2) = [ cmpq ~$1 ~~(compileOperand tm op), j Eq ~$$ s1, jmp ~$$ s2]
 
